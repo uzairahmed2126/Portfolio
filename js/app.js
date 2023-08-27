5feed9ee0dcd087b1b0013b597c06bf479000150
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-08-27 22:16:38 +0530

    nav bar

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const toggle = document.querySelector('.toggle')
 const mobile  = document.querySelector('.mobile')
+const navBar = document.querySelector('.nav-bar')
 toggle.addEventListener('click',()=>{
     tg()
 })
@@ -7,3 +8,20 @@ toggle.addEventListener('click',()=>{
 function tg(){
     mobile.classList.toggle('active')
 }
+
+navBar.addEventListener('input',()=>{
+    console.log('asd')
+})
+
+
+
+const BottomConatiner = document.querySelector('.paragraph')
+
+window.addEventListener('scroll', (event) => {
+  console.dir(window.scrollY)
+  if (window.scrollY > 200) {
+    navBar.classList.add('active')
+  } else {
+    navBar.classList.remove('active')
+  }
+})
