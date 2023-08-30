2972b589459245993601414b2125a437c1471bb4
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-08-30 15:57:40 +0530

    design

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 const toggle = document.querySelector('.toggle')
 const mobile = document.querySelector('.mobile')
 const navBar = document.querySelector('.nav-bar')
+const myname = document.querySelector('.my-name')
+const myFirstname= document.querySelector('.name1')
 toggle.addEventListener('click', () => {
   tg()
 })
@@ -12,16 +14,20 @@ function tg() {
 navBar.addEventListener('input', () => {
   console.log('asd')
 })
-
 window.addEventListener('scroll',AddNavFunctionality)
 function AddNavFunctionality() {
   if (window.scrollY > 65) {
     navBar.classList.add('active-nav')
-    navBar.style.background='#000221';
+    myFirstname.classList.add('my-name-active')
+    // navBar.style.background='#000221';
+    navBar.style.background='transparent';
     toggle.style.color='#f9f9f9'
+    myFirstname.style.color='#ffffffe9'
   } else {
     navBar.classList.remove('active-nav')
+    myFirstname.classList.remove('my-name-active')
     navBar.style.background='';
     toggle.style.color=''
+    myFirstname.style.color=''
   }
 }
