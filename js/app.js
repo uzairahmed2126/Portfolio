f2d43f387f842016dd9bd70c8586a767ba8df28e
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-09-09 02:30:30 +0530

    project completed

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 const toggle = document.querySelector('.toggle')
 const mobile = document.querySelector('.mobile')
 const navBar = document.querySelector('.nav-bar')
-const myname = document.querySelector('.my-name')
-const myFirstname= document.querySelector('.name1')
+const logo = document.querySelector('.logo')
+const myFirstname = document.querySelector('.name1')
 toggle.addEventListener('click', () => {
   tg()
 })
@@ -11,23 +11,19 @@ function tg() {
   mobile.classList.toggle('active')
 }
 
-navBar.addEventListener('input', () => {
-  console.log('asd')
-})
-window.addEventListener('scroll',AddNavFunctionality)
+
+window.addEventListener('scroll', AddNavFunctionality)
 function AddNavFunctionality() {
   if (window.scrollY > 65) {
-    navBar.classList.add('active-nav')
-    myFirstname.classList.add('my-name-active')
-    // navBar.style.background='#000221';
-    navBar.style.background='transparent';
-    toggle.style.color='#f9f9f9'
-    myFirstname.style.color='#ffffffe9'
+    navBar.classList.add('active-nav');
+    myFirstname.classList.add('my-name-active');
+    myFirstname.style.color = 'darkblue';
+    toggle.style.color='black'
   } else {
-    navBar.classList.remove('active-nav')
-    myFirstname.classList.remove('my-name-active')
-    navBar.style.background='';
-    toggle.style.color=''
-    myFirstname.style.color=''
+    navBar.classList.remove('active-nav');
+    myFirstname.classList.remove('my-name-active');
+    navBar.style.background = '';
+    toggle.style.color = '';
+    myFirstname.style.color = '';
   }
 }
